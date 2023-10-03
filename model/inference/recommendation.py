4ad5b36f421319db9caafc54fe138fa6d9c6e354
abilfad <m.nabilfad@gmail.com>
2023-10-03 09:14:38 +0700

    commiting other change

--- model/inference/recommendation.py
+++ model/inference/recommendation.py
@@ -1,31 +1,30 @@
-import os
-import pickle
-from pprint import pprint as pp
 from surprise import dump
+import os
+from pprint import pprint as pp
 
 
-def load_model(model_filename, verbose=False):
-    if verbose:
-        print(">> Loading dump")
+def load_model(model_filename):
+    print(">> Loading dump")
     from surprise import dump
     import os
     file_name = os.path.expanduser(model_filename)
     _, loaded_model = dump.load(file_name)
-    if verbose:
-        print(">> Loaded dump")
+    print(">> Loaded dump")
     return loaded_model
 
 
-def item_rating(model, user=0, item=0, print_output=False):
+def item_rating(model,user, item, verbose = False):
+
+
     uid = str(user)
     iid = str(item)
-    loaded_model = model
-    prediction = loaded_model.predict(user, item, verbose=True)
+    prediction = model.predict(user, item)
     rating = prediction.est
     details = prediction.details
     uid = prediction.uid
     iid = prediction.iid
     true = prediction.r_ui
+
     ret = {
         'user': user,
         'item': item,
@@ -35,11 +34,6 @@ def item_rating(model, user=0, item=0, print_output=False):
         'iid': iid,
         'true': true
     }
-    if print_output:
-        # pp (ret)
-        print('\n\n')
+
     return ret
 
-
-if __name__ == "__main__":
-    pass
--- model/test.py
+++ model/test.py
@@ -1,30 +1,42 @@
 from model.data_interface.data import *
 from model.inference.training import *
+from model.inference.recommendation import *
 from surprise import dump
-
 
 data = load_data(data_path="../data/dicoding_user_item_rating.gzip")
 print(data.head())
 
-
-data_input = data.loc[:,["user_id", "course_id", "rating"]]
+data_input = data.loc[:, ["user_id", "course_id", "rating"]]
 surprise_input = reader_data(data=data_input,
                              cols=["user_id", "course_id", "rating"],
                              scale=True,
-                             model = "surprise")
-
+                             model="surprise")
 
 print((type(surprise_input)))
-model_searched = model_search(data = surprise_input, cv = 3)
+model_searched = model_search(data=surprise_input, cv=3)
 print(model_searched)
 model_searched.to_csv("../backend/model.csv")
 
-params = fine_tuned_model(surprise_input, model = "SVD", cv = 5)
+params = fine_tuned_model(surprise_input, model="SVDpp", cv=5)
 
 print("Attempting to export the model...")
 
+print(params.best_params['rmse'])
 
-algo = SVD(**params.best_params['rmse'])
-dump.dump("../backend/model/model.pkl", algo=algo)
+algo = SVDpp(**params.best_params['rmse'])
+trainset = surprise_input.build_full_trainset()
+algo.fit(trainset)
+dump.dump(file_name="../backend/model/model.pickle", algo=algo)
 
+model_filename = "../backend/model/model.pickle"
+model = load_model(model_filename)
 
+cont_rec = []
+
+for i in data.course_id.unique():
+    rating = item_rating(model, user=data.iloc[100,0], item=f"{i}")['rating']
+    cont_rec.append((i, rating))
+
+recommendations = sorted(cont_rec, key=lambda x: x[1], reverse=True)
+
+print(recommendations)
